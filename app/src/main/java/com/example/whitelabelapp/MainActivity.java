633d388229c1ab322462b807960c0116d89ee65c
Ivamacio Magalhaes <ivan4341@gmail.com>
2017-12-05 13:24:46 -0500

    First commit

---
+++ app/src/main/java/com/example/whitelabelapp/MainActivity.java
@@ -0,0 +1,10 @@
+package com.example.whitelabelapp;
+
+import android.support.v7.app.AppCompatActivity;
+
+/**
+ * Created by ivamaciomagalhaes on 12/5/17.
+ */
+
+public class MainActivity extends AppCompatActivity {
+}
